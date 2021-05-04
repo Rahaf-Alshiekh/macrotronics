693a6c295ff0a54d122815a7b0cdeee153c26f7c
leen-96 <lyynall535@gmail.com>
2021-05-05 01:40:35 +0300

    Adding the brand page

---
+++ resources/views/Brand.blade.php
@@ -0,0 +1,192 @@
+    
+    @extends('layouts.master')
+    @section('content')
+        <div class="container">
+            {{-- section title --}}
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="main-tilte">
+                        <h1>  MACROTRONICS </h1>
+                        <i class="fa fa-hand-o-right"></i>
+                        <a href="#">BRANDS</a>
+                    </div>
+    
+                </div>
+            </div>
+    
+          {{-- ............................... --}}
+        <div class=" row">
+        
+           <div class="contentContainer">
+    
+                        <ul style="list-style: none; margin: 0; padding: 0;">
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                            
+                            <li class="manufacturers_logo">
+                                <img  src="/images/Apple_log.jpg" alt="3M" title="3M" width="324" height="260" />
+                            <a style="background-color:#ebebeb; font-weight:bold;" class="fl_left" href="#">3M</a>
+                            <a class="fl_right" href="#">18 products</a>
+                            </li>
+
+                        </ul>
+        </div>
+    
+    
+    
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        </div> {{-- end container --}}
+    
+    @endsection
+    
